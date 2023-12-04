53b1fce5667af6d4cd976335b76eef423d119861
vinsky001 <ekibet544@gmail.com>
2023-12-04 12:36:04 -0800

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+/* eslint-disable no-console */
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class DBClient {
+  constructor() {
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const dbUrl = `mongodb://${host}:${port}`;
+    this.connected = false;
+    this.client = new mongodb.MongoClient(dbUrl, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
